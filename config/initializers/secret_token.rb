daae6768a5196c4a04ac74b429e65452d4a1737b
kstefanie <kstefanie@gmail.com>
2013-02-18 19:30:47 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pipiwangdao::Application.config.secret_token = '90bff3b8538eff23602ac2533c46cefb86fb59d9614e56ba6f340c7be0d8d79d731e0de9203b7828489d4558b45045e6bc7dc899d03c3a7f94cb3ddcb1c1a230'
